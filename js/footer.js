a23960443bcd5484d30c6f5795f6e1e305f29a0d
DonCardenioVI <83748651+DonCardenioVI@users.noreply.github.com>
2024-08-09 07:56:57 -0500

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-var name = "doncardenio6";
-var domain = "gmail.com";
+var name = "PrinceZJ";
+var domain = "proton.me";
 var footer = document.getElementById("footer");
-footer.innerHTML = "<i><p>100% handcrafted 🖤 please share!</p><p><span  style='color:#61892F;'>"+name + "<span>@</span>" + domain+"</span>    (talk, troll, or do whatever to me)</p></i><hr><br><p>Copyright © 2023, 2024 The Last Writer</p>";
+footer.innerHTML = "<i><p>100% handcrafted 🖤 please share!</p><p><span  style='color:#61892F;'>"+name + "<span>@</span>" + domain+"</span>    (talk, troll, or do whatever to me)</p></i><hr><br><p>Copyright © 2024 Prince ZJ</p>";
