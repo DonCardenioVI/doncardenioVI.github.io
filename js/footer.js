5278b74e5cc770f764a1c7d5b0c14605070cfac4
DonCardenioVI <83748651+DonCardenioVI@users.noreply.github.com>
2024-08-09 07:57:12 -0500

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
 var name = "PrinceZJ";
 var domain = "proton.me";
 var footer = document.getElementById("footer");
-footer.innerHTML = "<i><p>100% handcrafted 🖤 please share!</p><p><span  style='color:#61892F;'>"+name + "<span>@</span>" + domain+"</span>    (talk, troll, or do whatever to me)</p></i><hr><br><p>Copyright © 2024 Prince ZJ</p>";
+footer.innerHTML = "<i><p>100% handcrafted 🖤 please share!</p><p><span  style='color:#61892F;'>"+name + "<span>@</span>" + domain+"</span></p></i><hr><br><p>Copyright © 2024 Prince ZJ</p>";
